7d31aef8876bc78249240c9149d190f5b598f56e
Asadulloh <asadullosadirdinov@gmail.com>
2024-04-02 18:09:25 +0500

    feat: implement workshop

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,15 @@ import tailwind from "@astrojs/tailwind";
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import icon from "astro-icon";
+import react from "@astrojs/react";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sitemap(), mdx(), tailwind(), icon()]
+  integrations: [sitemap(), mdx(), tailwind(), icon(), react()],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
